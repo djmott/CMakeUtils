fca90b84a910be78ee1da1313b95a1e7c7ab2022
David Mott <mott.david.j@gmail.com>
2018-10-26 04:37:34 -0500

    ..

---
+++ clang_tidy.cmake
@@ -0,0 +1,7 @@
+
+find_program(CMAKE_CXX_CLANG_TIDY NAMES clang_tidy)
+if (CMAKE_CXX_CLANG_TIDY)
+  list(APPEND CMAKE_CXX_CLANG_TIDY
+    "-checks=-*,readability-*,clang-analyzer-*,bugprone-*"
+  )
+endif()
--- cppcheck.cmake
+++ cppcheck.cmake
@@ -1,12 +1,11 @@
 
 find_program(CMAKE_CXX_CPPCHECK NAMES cppcheck)
 if (CMAKE_CXX_CPPCHECK)
-    list(
-        APPEND CMAKE_CXX_CPPCHECK 
-            "--enable=warning"
-            "--inconclusive"
-            "--force" 
-            "--inline-suppr"
-            "--suppressions-list=${CMAKE_SOURCE_DIR}/CppCheckSuppressions.txt"
+    list(APPEND CMAKE_CXX_CPPCHECK 
+      "--enable=warning"
+      "--inconclusive"
+      "--force" 
+      "--inline-suppr"
+      "--suppressions-list=${CMAKE_SOURCE_DIR}/CppCheckSuppressions.txt"
     )
 endif()
